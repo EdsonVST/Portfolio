f74cb592393078f7a2e04058766d254ebf91853c
Edson Vinicio Silva <edsonvinicio23p@gmail.com>
2026-01-12 23:12:26 -0300

    5.3.2
    Ajustando a responsividade

--- script.js
+++ script.js
@@ -69,3 +69,11 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
 });
+
+
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
